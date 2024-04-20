73bccc31a5f8a1e6101ab43e3ed0e26267daea2b
phanideva <phanisaisri@gmail.com>
2024-04-20 18:36:04 -0500

    configured settings for ocr signs and logos

--- tesseract/main.py
+++ tesseract/main.py
@@ -1,4 +1,5 @@
 import pytesseract
+from pytesseract import Output
 import PIL.Image
 import cv2
 
@@ -29,7 +30,31 @@ OCR Engine Mode
 3 Default, based on what is available.
 """
 
-myconfig = r"--psm 6 --oem 3"
+myconfig = r"--psm 11 --oem 3"
 
-text = pytesseract.image_to_string(PIL.Image.open("text.jpg"), config=myconfig)
-print(text)
+#text = pytesseract.image_to_string(PIL.Image.open("signs.jpg"), config=myconfig)
+#print(text)
+
+img = cv2.imread("logos.jpg")
+height, width, _ = img.shape
+
+#boxes = pytesseract.image_to_boxes(img, config=myconfig)
+##print(boxes)
+#for box in boxes.splitlines():
+#    box = box.split(" ")
+#    img = cv2.rectangle(img, (int(box[1]), height - int(box[2])), (int(box[3]), height - int(box[4])), (0, 255, 0), 2)
+
+## Image to data ##
+
+data = pytesseract.image_to_data(img, config=myconfig, output_type=Output.DICT)
+
+amount_boxes = len(data['text'])
+for i in range(amount_boxes):
+    if float(data['conf'][i]) > 20:
+        (x, y, width, height) = (data['left'][i], data['top'][i], data['width'][i], data['height'][i])
+        img = cv2.rectangle(img, (x, y), (x + width, y + height), (0, 255, 0), 2)
+        img = cv2.putText(img, data['text'][i], (x, y + height + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2, cv2.LINE_AA)
+        
+#print(data['text'])
+cv2.imshow("img", img)
+cv2.waitKey(0)
